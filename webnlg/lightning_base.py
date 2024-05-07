8bc61bab4559f54468bf5031c31eccf859234c18
Mongjin <30318269+Mongjin@users.noreply.github.com>
2024-05-07 15:45:08 +0900

    edit code for recent version of transformers and pytorch_lightning

--- webnlg/lightning_base.py
+++ webnlg/lightning_base.py
@@ -183,7 +183,7 @@ class BaseTransformer(pl.LightningModule):
         dataset_size = len(self.train_loader.dataset)
         return (dataset_size / effective_batch_size) * self.hparams.max_epochs
 
-    def setup(self, mode):
+    def setup(self, stage=None):
         #if mode == "fit":
         self.train_loader = self.get_dataloader("train", self.hparams.train_batch_size, shuffle=True)
 
